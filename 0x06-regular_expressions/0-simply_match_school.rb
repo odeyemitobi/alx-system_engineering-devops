0442958232677c0baf7a04aef0f01aae11e31696
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-02-27 19:28:28 +0100

    Task 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,13 +1,2 @@
 #!/usr/bin/env ruby
-
-require 'oniguruma'
-
-regex_pattern = /School/
-
-input_argument = ARGV[0]
-
-if input_argument =~ regex_pattern
-  puts "The input '#{input_argument}' matches the term 'School'."
-else
-  puts "The input '#{input_argument}' does not match the term 'School'."
-end
+puts ARGV[0].scan(/School/).join
